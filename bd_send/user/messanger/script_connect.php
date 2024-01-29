4318da4f89a3b41edb2dadd7664fd3f69b534635
Artemet <60576459+Artemet@users.noreply.github.com>
2024-01-29 22:05:31 +0200

    Delete bd_send directory

--- bd_send/user/messanger/script_connect.php
+++
@@ -1,4 +0,0 @@
-<script src="../page_js/messanger/chat_choice.js"></script>
-<script src="../page_js/messanger/chat.js"></script>
-<script src="../page_js/messanger/send_menu.js"></script>
-<script src="../page_js/messanger/smile_add.js"></script>
